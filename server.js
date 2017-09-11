890b82fedcea237fb9119f07ea64637b0767a2b5
Meredith H <meredithhazan@gmail.com>
2017-09-10 21:41:18 -0700

    server.js update for deployment

--- server.js
+++ server.js
@@ -83,6 +83,6 @@ app.post("/saved", function(req, res) {
 	});
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
 	console.log("App running on port 3000!");
 });
